960b5b25294d5c3752230497d34a73ee5cda56e1
Dheemanth Hassan Narasimhan <dheemanth.hn@gmail.com>
2016-01-19 12:33:10 -0800

    Update objectRecognition.java

--- objectRecognition.java
+++ objectRecognition.java
@@ -28,4 +28,34 @@ class demo implements ActionListener
 	j1= new JLabel("Select the directory");
 	j2= new JLabel("Select the File");
 	jt1= new JTextField();
+	jt2= new JTextField();
+	jb1= new JButton("Compare");            //  a button in GUI for Compare
+	jb2= new JButton("Browse");              //  button for browsing the directory
+	jb3= new JButton("Browse");              //  button for browsing the location of image
+	jb1.addActionListener(this);
+	jb2.addActionListener(this);
+	jb3.addActionListener(this);
 	
+	j1.setBounds(20,50,150,25);
+	c.add(j1);
+	jt1.setBounds(150,50,120,25);
+	c.add(jt1);
+	j2.setBounds(20,80,150,25);
+	c.add(j2);
+	jt2.setBounds(150,80,120,25);
+	c.add(jt2);
+	jb2.setBounds(300,50,120,25);
+	c.add(jb2);
+	jb3.setBounds(300,80,120,25);
+	c.add(jb3);
+	jb1.setBounds(200,250,120,25);
+	 c.add(jb1);
+	j.setSize(500,500);
+	j.setVisible(true);
+   }	
+
+
+ public void actionPerformed(ActionEvent ae)
+ {
+     if(ae.getSource()==jb2)
+ 
