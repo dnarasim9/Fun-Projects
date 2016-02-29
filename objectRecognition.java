08ca5b661eb8512cee9c821e28c25d19d494813f
Dheemanth Hassan Narasimhan <dheemanth.hn@gmail.com>
2016-02-28 16:41:15 -0800

    Update objectRecognition.java

--- objectRecognition.java
+++ objectRecognition.java
@@ -368,3 +368,443 @@ file = new PPMFile(f);
 
 //for filetering of images into jpeg or jpg
 class ImageFilter implements FileFilter
+{
+public boolean accept(File f) 
+{
+if (f.isDirectory()) 
+          			return true;
+       	}		
+
+ 	String extension = f.getName();
+      	int i = extension.lastIndexOf('.');
+
+       	if (i > 0 &&  i < extension.length() - 1)   
+        extension = extension.substring(i+1).toLowerCase();
+       
+       	if (extension != null) 
+{
+if ((extension.equals("ppm")) || (extension.equals("pnm")) || (extension.equals("jpg")) || 
+(extension.equals("jpeg")))
+              		    return true;
+            	} 
+else return false;
+           	return false;
+    }
+}
+
+
+/*--------Module 4: Face Bundle---------*/
+
+import java.io.*;
+import javax.swing.*;
+/**
+ * This bundle has all the relevant information to try to match an image
+ * The computation to be done (to find the submitted image in this
+ * bundles' face-space) is quite fast.
+ */
+public class FaceBundle implements Serializable, Comparable 
+{
+private double[] avgFace = null;
+private double[] cmpFace = null;
+private double[][] eigVector = null;
+private double[][] wk = null;
+private String[] id   = null;
+private transient double minD   = Double.MAX_VALUE;
+public int length = Integer.MIN_VALUE;
+private transient boolean computed = false;
+private transient int idx = Integer.MAX_VALUE;
+
+public FaceBundle(double[] avgF, double wk[][], double[][] eigV, String[] files) 
+{
+
+avgFace = new double[avgF.length];
+  	this.wk = new double[wk.length][wk[0].length];
+    	eigVector = new double[eigV.length][eigV[0].length];
+length = avgFace.length;
+
+    	System.arraycopy(avgF,0,this.avgFace,0,avgFace.length);
+    	System.arraycopy(eigV,0,this.eigVector,0,eigVector.length);
+    	System.arraycopy(wk,  0,this.wk, 0, wk.length);
+this.id = files;
+  }
+
+public void submitFace(byte[] face) 
+{
+for (int i = 0; i< length;i++)
+      		cmpFace[i] = (double) (face[i] & 0xFF);
+
+   	compute();
+  }
+
+
+public void submitFace(int[] face)
+{
+for (int i = 0; i< length;i++)
+      		cmpFace[i] = face[i];
+
+   	 compute();
+}
+
+public void submitFace(double[] face) 
+{
+this.cmpFace = face;
+compute();
+}
+ 
+ /**
+   * Clear the submitted image from the face-space object.
+ */
+public void clearFace()
+{
+cmpFace = null;
+    	computed = false;
+  	idx = Integer.MAX_VALUE;
+   	minD = Double.MAX_VALUE;
+}
+
+  /**
+   * The distance of how far away the submitted image is in this
+   */
+public double distance() 
+{
+return minD;
+}
+
+public String getID()
+{
+return this.id[idx];
+}
+
+public String[] getNames()
+{
+return id;
+}
+
+  /**
+   * Compare this face-space bundle to another. If this bundle has
+   * a smaller distance than the other, -1 is returned. 1 if its opposite.
+   * There is no checking if the other face-space bundle
+   * has computed its values for the same image.
+   */
+  
+
+public int compareTo(Object o)
+{
+if (((FaceBundle)o).minD > minD)
+    		return 1;
+  	if (((FaceBundle)o).minD < minD)
+return -1;
+return 0;
+}
+
+public String toString() 
+{
+if (computed)
+      		return "["+id[idx] + "] with "+minD;
+    	return "No image supplied";
+}
+
+private void compute() 
+{
+double[] inputFace = new double[length];
+  	int nrfaces = eigVector.length;
+    	int MAGIC_NR = wk[0].length;
+   	int j, pix, image;
+computed = false;
+   	
+try
+{ 	System.arraycopy(cmpFace,0,inputFace,0,length);	 }
+  	catch (ArrayIndexOutOfBoundsException e)
+   	{  	JOptionPane.showMessageDialog(null," Input image not of size 200x200 ","Error",1);
+    		System.exit(0);
+   	}
+   	for ( pix = 0; pix < inputFace.length; pix++)
+{
+        		inputFace[pix] = inputFace[pix] - avgFace[pix];
+  	  }
+
+double[] input_wk = new double[MAGIC_NR];
+    	double temp = 0;
+
+    	/* Subtract the image from the average image */
+
+ 	for (j = 0; j < MAGIC_NR; j++) 
+{
+    		temp = 0.0;
+      		for ( pix=0; pix <length; pix++)
+       		temp += eigVector[j][pix] * inputFace[pix];
+input_wk[j] = Math.abs( temp );
+    	}
+
+    /* weight vector computation
+     * Find the minimun distance from the input_wk as compared to wk
+     */
+
+double[] distance = new double[MAGIC_NR];
+    	double[] minDistance = new double[MAGIC_NR];
+  	idx = 0;
+    	for (image = 0; image < nrfaces; image++)
+{
+     		temp = 0.0;
+        		
+for (j = 0; j < MAGIC_NR; j++) 
+{
+        			distance[j] = Math.abs(input_wk[j] - wk[image][j]);
+               	}
+if (image == 0)
+          			System.arraycopy(distance,0,minDistance,0,MAGIC_NR);
+  		if (sum(minDistance) > sum(distance)) 
+{
+this.idx = image;
+          			System.arraycopy(distance,0,minDistance,0,MAGIC_NR);
+        		}
+ }
+
+
+    /*
+     * Normalize our minimum distance.
+     */
+if (max(minDistance) > 0.0)
+      		divide(minDistance, max(minDistance));
+minD = sum(minDistance);
+computed = true;
+}
+
+
+static void divide(double[] v, double b)
+{
+for (int i = 0; i< v.length; i++)
+      		v[i] = v[i] / b;
+}
+
+static double sum(double[] a) 
+{
+double b = a[0];
+ 	for (int i = 0; i < a.length; i++)
+    		b += a[i];
+return b;
+}
+
+static double max(double[] a) 
+{
+    double b = a[0];
+    for (int i = 0; i < a.length; i++)
+    if (a[i] > b) b = a[i];
+return b;
+}
+
+}
+
+
+/*---- Module 5 : Eigen Face Computation-----*/
+
+public class EigenFaceComputation
+{
+
+ private final static int MAGIC_NR = 11;
+
+
+public static FaceBundle submit(double[][] face_v, int width, int height, String[] id,
+ boolean debug) {
+
+int length = width*height;
+int nrfaces = face_v.length;
+int i, j, col,rows, pix, image;
+double temp = 0.0;
+double[][] faces = new double[nrfaces][length];
+double[] avgF = new double[length];
+
+   	 /*
+  	   Compute average face of all of the faces. 1xN^2
+    	 */
+
+  	for ( pix = 0; pix < length; pix++) 
+{
+     		temp = 0;
+      		for ( image = 0; image < nrfaces; image++) 
+{
+       			temp +=  face_v[image][pix];
+    		}
+    		avgF[pix] = temp / nrfaces;
+    	}
+ 
+	/* compute difference images*/
+
+for ( image = 0; image < nrfaces; image++) 
+{
+      	for ( pix = 0; pix < length; pix++)
+{
+      			face_v[image][pix] = face_v[image][pix] - avgF[pix];
+      		}
+   	 }
+
+Matrix faceM = new Matrix(face_v, nrfaces,length);
+Matrix faceM_transpose = faceM.transpose();
+Matrix covarM = faceM.times(faceM_transpose);
+
+    /*
+     Compute eigenvalues and eigenvector. Both are MxM
+    */
+
+EigenvalueDecomposition E = covarM.eig();
+
+  	double[] eigValue = diag(E.getD().getArray());
+double[][] eigVector = E.getV().getArray();
+
+ /*
+  * We only need the largest associated values of the eigenvalues.
+   	  * Thus we sort them (and keep an index of them)
+  	  */
+
+int[] index = new int[nrfaces];
+double[][] tempVector = new double[nrfaces][nrfaces];  /* Temporary new eigVector */
+
+   	for ( i = 0; i <nrfaces; i++) /* Enumerate all the entries */
+     		index[i] = i;
+
+    	doubleQuickSort(eigValue, index,0,nrfaces-1);
+int[] tempV = new int[nrfaces];
+  	for ( j = 0; j < nrfaces; j++)
+   		tempV[nrfaces-1-j] = index[j];
+index = tempV;
+
+for ( col = nrfaces-1; col >= 0; col --) 
+{
+for ( rows = 0; rows < nrfaces; rows++ )
+{
+       			tempVector[rows][col] = eigVector[rows][index[col]];
+}
+}
+ 	eigVector = tempVector;
+  	tempVector = null;
+  	eigValue = null;
+
+Matrix eigVectorM = new Matrix(eigVector, nrfaces,nrfaces);
+eigVector = eigVectorM.times(faceM).getArray();
+
+for ( image = 0; image < nrfaces; image++)
+{
+    		temp = max(eigVector[image]);
+for ( pix = 0; pix < eigVector[0].length; pix++)
+eigVector[image][pix] = Math.abs( eigVector[image][pix] / temp);
+}
+
+double[][] wk = new double[nrfaces][MAGIC_NR]; // M rows, 11 columns
+
+
+
+
+/*
+  	Compute wk.
+  	*/
+
+for (image = 0; image < nrfaces; image++) 
+{
+   		for (j  = 0; j <  MAGIC_NR; j++) 
+{
+     			temp = 0.0;
+    			for ( pix=0; pix< length; pix++)
+          				temp += eigVector[j][pix] * faces[image][pix];
+      			  wk[image][j] = Math.abs( temp );
+      		}
+  	  }
+
+FaceBundle b = new FaceBundle(avgF, wk, eigVector ,id);
+
+double[] inputFace = new double[length];
+    	System.arraycopy(faces[14],0,inputFace,0,length);
+
+return b;
+  }
+
+
+static double[] diag(double[][] m) 
+{
+
+double[] d = new double[m.length];
+  	for (int i = 0; i< m.length; i++)
+d[i] = m[i][i];
+  	return d;
+}
+
+ static void divide(double[] v, double b)
+{
+for (int i = 0; i< v.length; i++)
+ 		v[i] = v[i] / b;
+}
+static double sum(double[] a) 
+{
+double b = a[0];
+ 	for (int i = 0; i < a.length; i++)
+ 		b += a[i];
+return b;
+}
+
+static double max(double[] a) 
+{
+double b = a[0];
+for (int i = 0; i < a.length; i++)
+if (a[i] > b) b = a[i];
+return b;
+}
+
+// Quick Sort
+
+static void doubleQuickSort(double a[], int index[], int lo0, int hi0) 
+{
+     	int lo = lo0;
+      	int hi = hi0;
+        	double mid;
+
+      	if ( hi0 > lo0)
+{
+mid = a[ ( lo0 + hi0 ) / 2 ];
+while( lo <= hi ) 
+{
+ while( ( lo < hi0 ) && ( a[lo] < mid )) 
+           			 	 ++lo;
+                	                while( ( hi > lo0 ) && ( a[hi] > mid )) 
+                   			 --hi;
+
+                if( lo <= hi ) 
+{
+swap(a, index, lo, hi);
+                			++lo;
+                  			--hi;
+             			}	
+            		}
+if( lo0 < hi )
+{
+              			doubleQuickSort( a, index, lo0, hi );
+          		}
+           		if( lo < hi0 )
+{
+          			doubleQuickSort( a, index,lo, hi0 );
+        		}
+      	}
+  }
+
+/*--------- Module 6 : Maths---------*/
+
+public class Maths 
+{
+public static double hypot(double a, double b) 
+{
+double r;
+if (Math.abs(a) > Math.abs(b)) 
+{
+r = b/a;
+r = Math.abs(a)*Math.sqrt(1+r*r);
+  		} 
+else if (b != 0) 
+{
+        			r = a/b;
+        			r = Math.abs(b)*Math.sqrt(1+r*r);
+      		} 
+else 
+{
+r = 0.0;
+      		}
+   	return r;
+   	}
+}
